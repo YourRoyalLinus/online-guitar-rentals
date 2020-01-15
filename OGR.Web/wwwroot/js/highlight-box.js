834c34f0c8135686905d3ea281dd338787eceabd
YourRoyalLinus <52103944+YourRoyalLinus@users.noreply.github.com>
2020-01-14 19:34:20 -0500

    Relocated highlight box functions to separate file

---
+++ OGR.Web/wwwroot/js/highlight-box.js
@@ -0,0 +1,26 @@
+﻿//Electric Box
+$(document).ready(function () {
+    $("#electric").hover(function () {
+        $("#electric-cell .card-text").css({ "color": "rgba(255, 255, 255, 1)", "font-weight": "400" })
+    }, function () {
+        $("#electric-cell .card-text").css({ "color": "rgba(255, 255, 255, .5)", "font-weight": "300" });
+    });
+});
+
+//Bass Box
+$(document).ready(function () {
+    $("#bass").hover(function () {
+        $("#bass-cell .card-text").css({ "color": "rgba(255, 255, 255, 1)", "font-weight": "400" })
+    }, function () {
+        $("#bass-cell .card-text").css({ "color": "rgba(255, 255, 255, .5)", "font-weight": "300" });
+    });
+});
+
+//Acoustic Box
+$(document).ready(function () {
+    $("#acoustic").hover(function () {
+        $("#acoustic-cell .card-text").css({ "color": "rgba(255, 255, 255, 1)", "font-weight": "400" })
+    }, function () {
+        $("#acoustic-cell .card-text").css({ "color": "rgba(255, 255, 255, .5)", "font-weight": "300" });
+    });
+});
